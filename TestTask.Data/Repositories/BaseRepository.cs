d71fa8263d92fbc32609de7e9a562580d2ac43b8
Lekksha <bennykorovin@gmail.com>
2022-06-15 22:38:15 +0300

    OilInfoRepository is working

--- TestTask.Data/Repositories/BaseRepository.cs
+++ TestTask.Data/Repositories/BaseRepository.cs
@@ -5,7 +5,7 @@ namespace TestTask.Data
 {
     public class BaseRepository
     {
-        private const string _сonnString = "Server=sql5105.site4now.net;User ID=db_a88368_lekksha001_admin;Password=******;Database=CoCowork.DB;";
+        private const string _сonnString = "Server=sql5105.site4now.net;User ID=db_a88368_lekksha001_admin;Password=*****;Database=db_a88368_lekksha001;";
 
         protected IDbConnection ProvideConnection() => new SqlConnection(_сonnString);
     }
--- TestTask.Data/Repositories/OilInfoRepository.cs
+++ TestTask.Data/Repositories/OilInfoRepository.cs
@@ -8,18 +8,18 @@ namespace TestTask.Data
 		private const string _selectOilInfoProcedure = "SELECT pr.RunName, pr.Itemtag, pr.NPD, pp.RunLength, pp.LineWeight, pp.RunDiam, " +
 			"pf.PressureRating, pf.FluidCode, pf.Temp FROM PipingRun pr " +
 			"JOIN RunToFluid rf on pr.OID = rf.OIDFrom " +
-			"JOIN PipingFluid pf on rf.OIDTo = pf.OID" +
-			"JOIN RunToPhysical rp on rp.OIDFrom = pr.OID" +
+			"JOIN PipingFluid pf on rf.OIDTo = pf.OID " +
+			"JOIN RunToPhysical rp on rp.OIDFrom = pr.OID " +
 			"JOIN PipingPhysical pp on pp.OID = rp.OIDTo";
 
 		public List<OilInfo> GetOilInfo()
         {
 			using IDbConnection connection = ProvideConnection();
 
-			return connection
-				.Query<OilInfo>
-					(_selectOilInfoProcedure)
-				.ToList();
+			var oilInfoList = connection.Query<OilInfo>(_selectOilInfoProcedure)
+					.ToList();
+
+			return oilInfoList;
         }
 	}
 }
