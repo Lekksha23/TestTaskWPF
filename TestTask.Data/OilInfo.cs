6d3efd5434a80b6f7f023e8e9c12a9826f55d729
Lekksha <bennykorovin@gmail.com>
2022-06-15 18:27:35 +0300

    Added data layer

---
+++ TestTask.Data/OilInfo.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace TestTask.Data
+{
+    public class OilInfo
+    {
+        public string RunName { get; set; }
+        public string? Itemtag { get; set; }
+        public int NPD { get; set; }
+        public double? RunLength { get; set; }
+        public double? LineWeight { get; set; }
+        public double? RunDiam { get; set; }
+        public double? PressureRating { get; set; }
+        public string? FluidCode { get; set; }
+        public double? Temp { get; set; }
+    }
+}
---
+++ TestTask.Data/Repositories/BaseRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace TestTask.Data
+{
+    public class BaseRepository
+    {
+        private const string _сonnString = "Server=sql5105.site4now.net;User ID=db_a88368_lekksha001_admin;Password=******;Database=CoCowork.DB;";
+
+        protected IDbConnection ProvideConnection() => new SqlConnection(_сonnString);
+    }
+}
